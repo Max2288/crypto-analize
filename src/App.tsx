538d8e947efae35527f296d887e6b8a2cfd985a9
daniil <skeal.smirnov2004@mail.ru>
2024-01-10 21:02:41 +0300

    changed graphs

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
 import NavBarSearch from "actions/components/Navbar/NavbarSerach";
 import SinglePage from "actions/components/SinglePage/SinglePage";
 import BasicLineChartComponent, { BasicLineChartOptions } from 'actions/components/Charts/BasicLineChart/BasicLineChart';
-import WaterfallChartComponent from 'actions/components/Charts/WaterfallChart/WaterfallChart';
-import PieChartComponent from 'actions/components/Charts/PieChart/PieChart';
-import { useState, useEffect } from "react";
+import WaterfallChartComponent, { WaterfallChartOptions} from 'actions/components/Charts/WaterfallChart/WaterfallChart';
+import PieChartComponent, { PieChartOptions, PieChartData } from 'actions/components/Charts/PieChart/PieChart';
+import { useState, useEffect, CSSProperties } from "react";
 import LoginComponent from "actions/components/LoginAuth/LoginComponent";
 import RegisterComponent from "actions/components/LoginAuth/RegisterComponent";
 import { Routes, Route } from 'react-router-dom';
 import { Carousel } from 'react-responsive-carousel';
 import { fetchJsonData } from "actions/utils/post";
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import {data, pieChartOptions, waterfallChartOptions } from "data";
+import { data, dataTranslate } from "data";
 
 
 function App() {
   const [requestData, setRequestData] = useState(data[0])
   const [bsLineChartOpt, setChartOptions] = useState<BasicLineChartOptions>({});
+  const [pChartOpt, setPieChartOptions] = useState<PieChartOptions>({});
+  const [wfChartOpt, setWaterfallChartOptions] = useState<WaterfallChartOptions>({});
   const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
@@ -24,6 +26,124 @@ function App() {
       "Content-Type": "application/json",
       "Accept": "application/json"
     }
+
+    const fetchPieData = async () => {
+      try {
+        const jsonData = await fetchJsonData("https://seagulltech.ru/data/products/", "POST", "", headers);
+        const data: [PieChartData] = jsonData.data
+        const pieChartOptions: PieChartOptions = {
+          backgroundColor: '#2c343c',
+          title: {
+            text: 'Товары на складе',
+            left: 'center',
+            top: 20,
+            textStyle: {
+              color: '#ccc'
+            }
+          },
+          tooltip: {
+            trigger: 'item'
+          },
+          visualMap: {
+            show: false,
+            min: 600,
+            max: 600,
+            inRange: {
+              colorLightness: [0, 1]
+            }
+          },
+          series: [
+            {
+              name: 'Количество, шт.',
+              type: 'pie',
+              radius: '55%',
+              center: ['50%', '50%'],
+              data: data.sort(function (a, b) {
+                return a.value - b.value;
+              }),
+              roseType: 'radius',
+              label: {
+                color: 'rgba(255, 255, 255, 0.3)'
+              },
+              labelLine: {
+                lineStyle: {
+                  color: 'rgba(255, 255, 255, 0.3)'
+                },
+                smooth: 0.2,
+                length: 10,
+                length2: 20
+              },
+              itemStyle: {
+                color: '#c23531',
+                shadowBlur: 200,
+                shadowColor: 'rgba(0, 0, 0, 1.5)'
+              },
+              animationType: 'scale',
+              animationEasing: 'elasticOut',
+              animationDelay: () => Math.random() * 200
+            }
+          ]
+        };
+        setPieChartOptions(pieChartOptions);
+      } catch (error) {
+        console.error("Ошибка:", error);
+      }
+    };
+    fetchPieData();
+
+    const fetchWaterfallData = async () => {
+      try {
+        const jsonData = await fetchJsonData("https://seagulltech.ru/data/waterfall/", "POST", "", headers);
+        const waterfallChartOptions: WaterfallChartOptions = {
+          title: {
+            text: 'Прибыль',
+            subtext: 'Каждый месяц',
+            padding: 20,
+          },
+          tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+              type: 'shadow'
+            },
+            formatter: function (params: any) {
+              var tar = params[0];
+              return tar.name + '<br/>' + tar.seriesName + ' : ' + tar.value;
+            }
+          },
+          grid: {
+            left: '10%',
+            right: '10%',
+            bottom: '6%',
+            containLabel: true,
+          },
+          xAxis: {
+            type: 'category',
+            splitLine: { show: false },
+            data: jsonData.xAxis
+          },
+          yAxis: {
+            type: 'value'
+          },
+          series: [
+            {
+              name: 'Значение',
+              type: 'bar',
+              stack: 'Total',
+              label: {
+                show: true,
+                position: 'inside'
+              },
+              data: jsonData.series
+            }
+          ]
+        }
+        setWaterfallChartOptions(waterfallChartOptions);
+      } catch (error) {
+        console.error("Ошибка:", error);
+      }
+    };
+    fetchWaterfallData();
+
     const fetchData = async () => {
       try {
         const jsonData = await fetchJsonData(
@@ -67,6 +187,15 @@ function App() {
     setCurrentIndex(index);
   };
 
+  const arrowStyles: CSSProperties = {
+    position: 'absolute',
+    zIndex: 2,
+    top: 'calc(50% - 15px)',
+    width: 30,
+    height: 30,
+    cursor: 'pointer',
+  };
+
   return (
     <>
       <NavBarSearch />
@@ -83,20 +212,34 @@ function App() {
                 showArrows={true}
                 selectedItem={currentIndex}
                 onChange={handleCarouselChange}
+                renderArrowNext={(onClickHandler, hasNext, label) =>
+                  hasNext && (
+                    <button type="button" onClick={onClickHandler} title={label} style={{ ...arrowStyles, right: 15 }}>
+                      +
+                    </button>
+                  )
+                }
+                renderArrowPrev={(onClickHandler, hasPrev, label) =>
+                  hasPrev && (
+                    <button type="button" onClick={onClickHandler} title={label} style={{ ...arrowStyles, left: 15 }}>
+                      -
+                    </button>
+                  )
+                }
               >
                 {
                   data.map(
-                    (item, index) => (
+                    (item: string, index) => (
                       <div key={index}>
-                        <h2>{item}</h2>
+                        <h2>{dataTranslate[index]}</h2>
                       </div>
                     )
                   )
                 }
               </Carousel>
               <BasicLineChartComponent option={bsLineChartOpt} width="100%" height="250px" />
-              <WaterfallChartComponent option={waterfallChartOptions} width="100%" height="400px" />
-              <PieChartComponent option={pieChartOptions} width="100%" height="400px" />
+              <WaterfallChartComponent option={wfChartOpt} width="100%" height="400px" />
+              <PieChartComponent option={pChartOpt} width="100%" height="400px" />
             </>
           }
         />
--- src/actions/utils/post.ts
+++ src/actions/utils/post.ts
@@ -1,5 +1,3 @@
-// TODO: поменял расширение файла на .ts, так как в тут полностью отсуствует верстка
-
 export const fetchJsonData = async (url: string, method: string, requestData: any, headers: any) => {
     try {
         const response = await fetch(url, {
@@ -20,6 +18,7 @@ export const fetchJsonData = async (url: string, method: string, requestData: an
         throw error;
     }
 };
+
 export const fetchEncodedData = async (url: string, method: string, requestData: any, headers: any) => {
     try {
         const response = await fetch(url, {
